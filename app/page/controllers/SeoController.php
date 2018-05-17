612a5adfbe210e66ae1f546deaca58e155bac37f
LiChunHua <lichunhua@medatc.com>
2018-05-17 11:41:43 +0800

    x

--- app/page/controllers/SeoController.php
+++ app/page/controllers/SeoController.php
@@ -5,16 +5,25 @@ use WS;
 
 class SeoController extends \module\core\component\Controller
 {
-    public function actionIndex($path = null)
+    public function actionIndex($path = null, $area)
     {
         if ($path && WS::$app->request->isPost) {
             $data = WS::$app->request->post('data');
 
-            $seoMeta = \models\SiteSeoMeta::findOne($path);
+            $seoMeta = \models\SiteSeoMeta::find()->where(['path' => $path, 'area_id' => $area])->one();
             if (!$seoMeta) {
                 $seoMeta = new \models\SiteSeoMeta();
+                $seoMeta->area_id = $area;
                 $seoMeta->path = $path;
             }
+            foreach ($data as $idx => $field) {
+                if (is_array($field) && count($field) === 2) {
+                    $data[$idx] = array_map(function ($val) {
+                        return empty($val) ? '.' : $val;
+                    }, $field);
+                }
+            }
+
             $seoMeta->setAttributes($data, false);
             $seoMeta->save();
 
@@ -24,11 +33,12 @@ class SeoController extends \module\core\component\Controller
         }
 
         $configs = include(APP_ROOT.'/../houses/config/metas.configs.php');
-        $entity = \models\SiteSeoMeta::findOneAsArray($path);
+        $entity = \models\SiteSeoMeta::findOneAsArray($area, $path);
 
-        $this->view->setActiveMenuId('seo');
+        $this->view->setActiveMenuId('seo-'.$area);
 
         return $this->render('index.phtml', [
+            'area' => $area,
             'currentPath' => $path,
             'configs' => $configs,
             'entity' => $entity
